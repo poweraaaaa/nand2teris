e3e6c6f1746b542ba54e938af6ca7c9088e16152
poweraaaaa <164000596+poweraaaaa@users.noreply.github.com>
2024-10-09 19:06:38 +0800

    Add files via upload

---
+++ 7/一阶段/VMtranslator1.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "Parser.hpp"
+#include "CodeWrite.hpp"
+#include<string.h>
+using std::string;
+
+class translator {
+public:
+	translator(ifstream& ifile, string iname, ofstream& ofile, string oname) :
+		ifile(ifile), iname(iname), ofile(ofile), oname(oname), parser(ifile) ,codewrite(ofile){};
+	void trans();
+
+private:
+	parser parser;
+	codewriter codewrite;
+	ifstream& ifile;
+	ofstream& ofile;
+	string iname, oname,command;
+	int index=0;
+};
+
+void translator::trans() {
+	while (parser.hasMoreCommands())
+	{
+		if (parser.advance())
+		{	
+			if (parser.commandType()==VMtype::C_ARITHMETIC)
+			{
+				command = parser.arg1();
+				codewrite.writeArthmelt(command);
+			}
+			else
+			{
+				index = parser.Arg2();
+				codewrite.WritePushPop(parser.commandType(), "constant", index);
+			}
+		}
+	}
+}
